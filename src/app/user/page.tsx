c716c521ffe5c096142c1418b5b0089d49fe3175
keisuke1213 <mayakufu2694@outlook.jp>
2024-06-08 22:04:59 +0900

    ユーザー登録ページの作成

---
+++ src/app/user/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import {UserRegisterForm} from './user_register_form';
+
+
+export default function UserRegisterPage() {
+    
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <UserRegisterForm />
+        </div>
+    )
+}
